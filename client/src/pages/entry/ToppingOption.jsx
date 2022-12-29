c79678a6a2bace58120f9cd2bc2561ee17152d0a
sleeplesseditor <josh@bryson.me.uk>
2022-12-29 19:48:04 +0000

    Toppings checkbox component expanded

--- client/src/pages/entry/ToppingOption.jsx
+++ client/src/pages/entry/ToppingOption.jsx
@@ -1,9 +1,23 @@
-import Col from 'react-bootstrap/Col';
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
-export default function ToppingOption({ name, imagePath }){
-    return (
-        <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
-            <img style={{ width: '75%' }} src={`http://localhost:3030/${imagePath}`} alt={`${name} topping`} />
-        </Col>
-    )
+export default function ToppingOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails();
+  const handleChange = (e) => {
+    updateItemCount(name, e.target.checked ? 1 : 0, "toppings");
+  };
+
+  return (
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+      <img
+        style={{ width: "75%" }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} topping`}
+      />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type="checkbox" onChange={handleChange} label={name} />
+      </Form.Group>
+    </Col>
+  );
 }
--- client/src/pages/entry/tests/totalUpdates.test.jsx
+++ client/src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,11 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
-import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 
 test("update scoop subtotal when scoops change", async () => {
   const user = userEvent.setup();
-  render(<Options optionsType="scoops" />, { wrapper: OrderDetailsProvider });
+  render(<Options optionsType="scoops" />);
 
   const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
   expect(scoopsSubtotal).toHaveTextContent("0.00");
@@ -25,4 +24,27 @@ test("update scoop subtotal when scoops change", async () => {
   await user.type(chocolateInput, "2");
 
   expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
+
+test("update toppings subtotal when toppings change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionsType="toppings" />);
+
+  const toppingsTotal = screen.getByText("Toppings total: $", { exact: false });
+  expect(toppingsTotal).toHaveTextContent("0.00");
+
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(cherriesCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+
+  const hotFudgeCheckbox = await screen.findByRole("checkbox", {
+    name: "Hot fudge",
+  });
+  await user.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent("3.00");
+
+  await user.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
 });
