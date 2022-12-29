c55323303b92fdac6fbf56dc417b316e34691f86
sleeplesseditor <josh@bryson.me.uk>
2022-12-29 18:39:59 +0000

    OptionsSummary component updated

--- client/src/pages/entry/Options.jsx
+++ client/src/pages/entry/Options.jsx
@@ -1,42 +1,48 @@
-import * as React from 'react';
-import axios from 'axios';
-import ScoopOption from './ScoopOption';
-import ToppingOption from './ToppingOption';
-import Row from 'react-bootstrap/Row';
-import AlertBanner from '../common/AlertBanner';
-import { pricePerItem } from '../../constants';
-import { formatCurrency } from '../../utilities';
+import * as React from "react";
+import axios from "axios";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { formatCurrency } from "../../utilities";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
-export default function Options({optionType}){
-    const [items, setItems] = React.useState([]);
-    const [error, setError] = React.useState(false);
+export default function Options({ optionType }) {
+  const [items, setItems] = React.useState([]);
+  const [error, setError] = React.useState(false);
+  const { totals } = useOrderDetails();
 
-    React.useEffect(() => {
-        axios.get(`http://localhost:3030/${optionType}`)
-            .then(response => setItems(response.data))
-            .catch(error => setError(true))
-    }, [optionType]);
+  React.useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => setError(true));
+  }, [optionType]);
 
-    if(error) {
-        <AlertBanner />
-    }
+  if (error) {
+    <AlertBanner />;
+  }
 
-    const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
-    const title = optionType[0].toUpperCase() + optonType.slice(1).toLowerCase();
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
-    const optionItems = items.map(item => (
-        <ItemComponent
-            key={item.name}
-            imagePath={item.imagePath}
-            name={item.name}
-        />
-    ))
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      imagePath={item.imagePath}
+      name={item.name}
+    />
+  ));
 
-    return (
-        <>
-            <h2>{title}</h2>
-            <p>{formatCurrency(pricePerItem[optionType])} each</p>
-            <Row>{optionItems}</Row>
-        </>
-    );
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
---
+++ client/src/pages/summary/OrderSummary.jsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import SummaryForm from "./SummaryForm";
+import { useOrderDetails } from "../../contexts/OrderDetails";
+import { formatCurrency } from "../../utilities";
+
+export default function OrderSummary() {
+  const { totals, optionCounts } = useOrderDetails();
+
+  const scoopArr = Object.entries(optionCounts.scoops);
+  const scoopList = scoopArr.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+
+  const toppingsArr = Object.keys(optionCounts.toppings);
+  const toppingsList = toppingsArr.map((key) => <li key={key}>{key}</li>);
+
+  return (
+    <div>
+      <h1>Order Summary</h1>
+      <h2>Scoops: {formatCurrency(totals.scoops)}</h2>
+      <ul>{scoopList}</ul>
+      <h2>Toppings: {formatCurrency(totals.toppings)}</h2>
+      <ul>{toppingsList}</ul>
+      <SummaryForm />
+    </div>
+  );
+}
